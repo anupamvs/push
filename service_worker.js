4a49b1b62855fa59add614bb288fcc721385bb99
Anupam <anupamvns0099@gmail.com>
2022-01-06 16:41:56 +0530

    update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -8,19 +8,7 @@ self.addEventListener('push', function(e) {
   }
 
   var options = {
-    body: body,
-    icon: 'images/notification-flat.png',
-    vibrate: [100, 50, 100],
-    data: {
-      dateOfArrival: Date.now(),
-      primaryKey: 1
-    },
-    actions: [
-      {action: 'explore', title: 'Explore this new world',
-        icon: 'images/checkmark.png'},
-      {action: 'close', title: 'I don\'t want any of this',
-        icon: 'images/xmark.png'},
-    ]
+    body: body
   };
   e.waitUntil(
     self.registration.showNotification('Push Notification', options)
