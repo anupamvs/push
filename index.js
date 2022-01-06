ff444461810d53e4025ce51af530bbc3ff6668e4
Anupam <anupamvns0099@gmail.com>
2022-01-06 15:37:42 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
-async function subscribe() {
-  let sw = await navigator.serviceWorker.ready;
-  let push = await sw.pushManager.subscribe({
-    userVisibleOnly: true,
-    applicationServerKey:
-      'BJJGXe_UI1N4uuyLGwfXQ1p3LUPYQnSlo9K_QtMAmRBsIYYoiqtW6gppS8mg_TnRRktW37hnPgAa1s_ncB9yBIU'
-  });
-  console.log(JSON.stringify(push));
-}
-
 if ('serviceWorker' in navigator) {
   addEventListener('load', async () => {
     let sw = await navigator.serviceWorker.register('./service_worker.js');
     console.log(sw);
   });
+}
+
+function subscribe() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.ready.then(function(reg) {
+
+      reg.pushManager.subscribe({
+        userVisibleOnly: true
+      }).then(function(sub) {
+        console.log(sub);
+      }).catch(function(e) {
+        if (Notification.permission === 'denied') {
+          console.warn('Permission for notifications was denied');
+        } else {
+          console.error('Unable to subscribe to push', e);
+        }
+      });
+    })
+  }
 }
